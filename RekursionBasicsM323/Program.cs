2b5ebc87b4275e60a35130d678ec8ed09b9a1394
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-09-06 11:51:04 +0200

    for loop version of some prime alg done

--- RekursionBasicsM323/Program.cs
+++ RekursionBasicsM323/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace RekursionBasicsM323 {
+﻿using System.Collections;
+
+namespace RekursionBasicsM323 {
   class Program {
     static void EinMalEinsFor() {
       for (int i = 1; i <= 10; i++) {
@@ -20,10 +22,41 @@
       return $"{EinMalEinsOuterRecursive(maxw, maxh - 1)}\n{EinMalEinsInnerRecursive(maxw, maxh, "")}";
     }
 
+    // TODO: Calculate prime numbers from 1 to n (excluding 1)
+    // For-loop solution: Time complexity: O(n * sqrt(n))
+    static List<int> PrimesToNForLoop(int n) {
+      List<int> primes = new List<int>();
+      primes.Add(2);
+      for (int i = 3; i < n; i++) {
+        bool isPrime = true;
+        foreach (int prime in primes) {
+          if (i % prime == 0) {
+            isPrime = false;
+            break;
+          }
+        }
+
+        if (isPrime) primes.Add(i);
+      }
+
+      return primes;
+    }
+
+    static void PrintIntList(List<int> ls) {
+      Console.Write("[");
+      foreach (int i in ls) {
+        Console.Write($"{i}, ");
+      }
+
+      Console.WriteLine("\b\b]");
+    }
+
+
     static void Main(String[] args) {
       // EinMalEinsFor();
-      Console.WriteLine(EinMalEinsOuterRecursive(15, 15));
-      
+      // Console.WriteLine(EinMalEinsOuterRecursive(15, 15));
+      PrintIntList(PrimesToNForLoop(100));
+      Console.WriteLine(PrimesToNForLoop(100).Count);
     }
   }
 }
